cce93a2fe4000cd80d728e6240fe86826584a2a1
nikolaidis_nikos <nikos.gate4.nik@gmail.com>
2021-12-20 12:29:31 +0200

    Add License Headers

--- src/main/java/gr/nikos/smartclideTDPrincipal/Analysis/Metric.java
+++ src/main/java/gr/nikos/smartclideTDPrincipal/Analysis/Metric.java
@@ -1,5 +1,15 @@
 package gr.nikos.smartclideTDPrincipal.Analysis;
 
+/*
+ * Copyright (C) 2021 UoM - University of Macedonia
+ * 
+ * This program and the accompanying materials are made available under the 
+ * terms of the Eclipse Public License 2.0 which is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ * 
+ * SPDX-License-Identifier: EPL-2.0
+ */
+
 public class Metric {
 	
 	private String name;
