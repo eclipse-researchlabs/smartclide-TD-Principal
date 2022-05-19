737515b931874ee613f45ac7fe73143d361a204d
Nikos Nikolaidis <nikos.gate4.nik@gmail.com>
2022-05-19 13:34:53 +0300

    Remove config of sonar-scanner.properties

--- src/main/java/gr/nikos/smartclideTDPrincipal/Analysis/AnalysisService.java
+++ src/main/java/gr/nikos/smartclideTDPrincipal/Analysis/AnalysisService.java
@@ -45,13 +45,6 @@ public class AnalysisService {
 	public String startNewAnalysis(RequestBodyAnalysis requestBodyAnalysis) {
 		String report="";
         try {
-            //setup sonarqube instance to sonarscanner
-            FileWriter fstream = new FileWriter("/sonar-scanner-4.6.2.2472-linux/conf/sonar-scanner.properties", false);
-            BufferedWriter out = new BufferedWriter(fstream);
-            out.write("sonar.host.url="+sonarQubeUrl);
-            out.close();
-            report+="sonarQubeUrl:"+sonarQubeUrl+"; ";
-
 			//mkdir
 			ProcessBuilder pbuilder = new ProcessBuilder("bash", "-c", "mkdir -p tmp");
 	        Process p = pbuilder.start();
