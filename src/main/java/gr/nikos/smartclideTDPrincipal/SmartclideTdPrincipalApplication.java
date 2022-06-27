e1df87fb7360802b8961c3439e6e9791d69e1a7d
Philippe Krief <philippe.krief@eclipse-foundation.org>
2022-06-27 17:05:45 +0200

    Revert "Revert "Added missing copyrights and reformated existing copyrights""
    This reverts commit 6007e5aad3e39993c24860a8eb69ad7264308a59.

--- src/main/java/gr/nikos/smartclideTDPrincipal/SmartclideTdPrincipalApplication.java
+++ src/main/java/gr/nikos/smartclideTDPrincipal/SmartclideTdPrincipalApplication.java
@@ -1,14 +1,14 @@
-package gr.nikos.smartclideTDPrincipal;
-
-/*
- * Copyright (C) 2021 UoM - University of Macedonia
+/*******************************************************************************
+ * Copyright (C) 2021-2022 UoM - University of Macedonia
  * 
- * This program and the accompanying materials are made available under the 
- * terms of the Eclipse Public License 2.0 which is available at
- * https://www.eclipse.org/legal/epl-2.0/
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
  * 
  * SPDX-License-Identifier: EPL-2.0
- */
+ ******************************************************************************/
+package gr.nikos.smartclideTDPrincipal;
+
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
